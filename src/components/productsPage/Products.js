62eeac4c7beb590a0df3cb40e97428c94d02b096
Mamun746 <abdullahalmamun914@gmail.com>
2020-02-24 18:57:41 +0600

    modify files

--- src/components/productsPage/Products.js
+++ src/components/productsPage/Products.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Title from '../Title'
+import {ProductConsumer} from '../../context/context'
+import Product from '../Product'
 
 export default function Products() {
     return (
-        <div>
-            our products
-        </div>
+        <ProductConsumer>
+            {
+                value=>{
+                    const {filteredProducts}=value
+                    return <section className="py-5">
+                    <div className="container">
+                    <Title title='our products' center/>
+                    <div className='row py-5'>
+                    {
+                        filteredProducts.map((product)=>{
+                                return <Product key={product.id} product={product}/>
+                        })
+                    }                    
+                    </div>
+                    </div>
+                    
+                    </section>
+                }
+            }
+        </ProductConsumer>
     )
 }
